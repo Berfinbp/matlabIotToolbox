35d257724dd6410eb1b6d52c082ec42f85782809
Berfinbp <108139077+Berfinbp@users.noreply.github.com>
2024-12-17 19:57:50 +0300

    Add files via upload

---
+++ New Folder/myfolder1/myHumiditysensor.m
@@ -0,0 +1,5 @@
+function outputArg1 = myHumiditysensor(inputArg1,inputArg2)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1 * inputArg2;
+end
---
+++ myProd.m
@@ -0,0 +1,5 @@
+function outputArg1 = myProd(inputArg1,inputArg2)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1 * inputArg2;
+end
